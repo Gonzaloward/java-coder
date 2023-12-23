3c9614980d97e54eda8335147fcacf279b080ac7
Elgonnza! <wardgonzalo15@gmail.com>
2023-12-23 15:51:44 -0300

    funciones

--- Usuario.js
+++ Usuario.js
@@ -1,21 +1,49 @@
+
+
+// funcion principal iniciar sesion
+
+
+function iniciarSesion(intentosMaximos, usuario, clave) {
+    for (var intento = 1; intento <= intentosMaximos; intento++) {
+        var usuarioIng = prompt("Ingrese su nombre de usuario:");
+        var claveIng = prompt("Ingrese su clave:");
+
+        if (verificarCredenciales(usuarioIng, claveIng, usuario, clave)) {
+            mostrarMensaje("Inicio de sesión exitoso");
+            break;
+        } else {
+            mostrarMensaje("Nombre de usuario o clave incorrectos. Intento " + intento + " de " + intentosMaximos);
+        }
+
+        if (intento === intentosMaximos) {
+            mostrarMensaje("Número máximo de intentos alcanzado. La página se cerrará.");
+            cerrarVentana();
+        }
+    }
+}
+
+
+//funciones
+
+
+function verificarCredenciales(usuarioIng, claveIng, usuario, clave) {
+    return usuarioIng === usuario && claveIng === clave;
+}
+
+function mostrarMensaje(mensaje) {
+    alert(mensaje);
+}
+
+function cerrarVentana() {
+    window.close();
+}
+
+
+// variables
+
+
 var intentosMaximos = 5;
 var usuario = "#01";
 var clave = "Mn12345";
 
-for (var intento = 1; intento <= intentosMaximos; intento++) {
-    var usuarioIng = prompt("Ingrese su nombre de usuario:");
-    var claveIng = prompt("Ingrese su clave:");
-
-    if (usuarioIng === usuario && claveIng === clave) {
-        alert("Inicio de sesion exitoso");
-        break;
-    } else {
-        alert("Nombre de usuario o clave incorrectos. Intento " + intento + " de " + intentosMaximos);
-    }
-
-    if (intento === intentosMaximos) {
-        alert("Numero maximo de intentos alcanzado. Cierre la ventana.");
-    }
-}
-
-    
+iniciarSesion(intentosMaximos, usuario, clave);
